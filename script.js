a88ea60ca76661f1b1431cff55b2ad57c3bc5428
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-06-22 04:49:32 -0300

    Added food function

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -20,10 +24,15 @@ function criarCobrinha() {
     }
 }
 
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
 
 function update(event) {
-    if (event.keyCode == 37 && direction != "right") direction = "letf";
+    if (event.keyCode == 37 && direction != "right") direction = "left";
     if (event.keyCode == 38 && direction != "down") direction = "up";
     if (event.keyCode == 39 && direction != "left") direction = "right";
     if (event.keyCode == 40 && direction != "up") direction = "down";
@@ -37,20 +46,21 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
-    let snakex = snake[0].x;
-    let snakey = snake[0].y;
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
 
-    if (direction == "right") snakex += box;
-    if (direction == "left") snakex -= box;
-    if (direction == "up") snakey -= box;
-    if (direction == "down") snakey += box;
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
 
     snake.pop();
 
     let newHead = {
-        x: snakex,
-        y: snakey
+        x: snakeX,
+        y: snakeY
     }
 
     snake.unshift(newHead);
